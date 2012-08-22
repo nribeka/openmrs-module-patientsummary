c41f5fca731a15cca127371168412891c55ed6d6
Michael Seaton <mseaton@pih.org>
2012-08-22 14:20:39 -0400

    Initial PoC showing use of Velocity for more dynamic templating

--- api/src/main/java/org/openmrs/module/patientsummary/PatientSummaryReportRenderer.java
+++ api/src/main/java/org/openmrs/module/patientsummary/PatientSummaryReportRenderer.java
@@ -15,12 +15,26 @@ package org.openmrs.module.patientsummary;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.log.CommonsLogLogChute;
 import org.openmrs.annotation.Handler;
 import org.openmrs.module.reporting.common.Localized;
+import org.openmrs.module.reporting.dataset.DataSet;
+import org.openmrs.module.reporting.dataset.DataSetRow;
+import org.openmrs.module.reporting.evaluation.EvaluationUtil;
 import org.openmrs.module.reporting.report.ReportData;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.ReportDesignResource;
 import org.openmrs.module.reporting.report.renderer.RenderingException;
 import org.openmrs.module.reporting.report.renderer.ReportRenderer;
 import org.openmrs.module.reporting.report.renderer.TextTemplateRenderer;
@@ -42,6 +56,53 @@ public class PatientSummaryReportRenderer extends TextTemplateRenderer {
 	 * @see ReportRenderer#render(ReportData, String, OutputStream)
 	 */
 	public void render(ReportData reportData, String argument, OutputStream out) throws IOException, RenderingException {
-		super.render(reportData, argument, out);
+		
+		Writer pw = null;
+		
+		try {
+			ReportDesign design = getDesign(argument);
+			ReportDesignResource r = getTemplate(design);
+			String templateContents = new String(r.getContents(), "UTF-8");
+			
+			Map<String, Object> replacements = getBaseReplacementData(reportData, design);
+			
+			for (String dsName : reportData.getDataSets().keySet()) {
+				DataSet ds = reportData.getDataSets().get(dsName);
+				int num = 0;
+				for (DataSetRow row : ds) {
+					if (num++ > 0) {
+						throw new RuntimeException("Currently only datasets with one row are supported.");
+					}
+					replacements.putAll(getReplacementData(reportData, design, dsName, row));
+				}
+			}
+			
+			VelocityEngine ve = new VelocityEngine();
+			ve.setProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS, "org.apache.velocity.runtime.log.CommonsLogLogChute");
+			ve.setProperty(CommonsLogLogChute.LOGCHUTE_COMMONS_LOG_NAME, "patientsummary_velocity");
+			ve.init();
+			VelocityContext velocityContext = new VelocityContext();
+			velocityContext.put("df", new SimpleDateFormat("dd/MMM/yyyy"));
+			
+			for (Map.Entry<String, Object> e : replacements.entrySet()) {
+				velocityContext.put(e.getKey().replace(".", "-"), e.getValue());
+			}
+			
+			StringWriter writer = new StringWriter();
+			ve.evaluate(velocityContext, writer, getClass().getName(), templateContents);
+			templateContents = writer.toString();
+			
+			String prefix = getExpressionPrefix(design);
+			String suffix = getExpressionSuffix(design);
+			
+			pw = new OutputStreamWriter(out,"UTF-8");
+			pw.write(EvaluationUtil.evaluateExpression(templateContents, replacements, prefix, suffix).toString());
+		}
+		catch (Exception e) {
+			throw new RenderingException("Unable to render results due to: " + e, e);
+		}
+		finally {
+			pw.close();
+		}
 	}
 }
